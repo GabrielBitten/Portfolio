b222574cfd275d3447e964f50c6ff234885e4010
Gabriel Bittencourt Camillo de Souzae <Gabrielbcds40@gmail.com>
2024-08-18 20:36:23 -0300

    só um teste

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#inicio-tela')
+    header.classList.toggle('scrolling', window.scrollY > 100)
+})
+
+
+
+//function typeWriter(elemento){
+ //   const textoArray = elemento.innerHTML.split('');
+ //   elemento.innerHTML= '';
+ //   textoArray.forEach((letra,i) => {
+ //       setTimeout(()=> elemento.innerHTML += letra, 40 * i);
+        
+//    });
+    
+
+//}
+//const titulo = document.querySelector('#apresentacao');
+//typeWriter(titulo);
+
+
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('.info-box',{duration:1700});
+sr.reveal('#sobre h1',{duration:1700});
+sr.reveal('.tec-conteiner-1',{duration:1700});
+sr.reveal('.tec-conteiner-2',{duration:1700});
+sr.reveal('#tecnologias h1',{duration:1700});
+sr.reveal('#tecnologias p',{duration:1700});
+sr.reveal('.project-conteiner',{duration:1700});
+sr.reveal('#projetos h1',{duration:1700});
+sr.reveal('#projetos p',{duration:1700});
+
+/*------------------------------------------------------------------------------------*/
+
+
+const barras = document.querySelector(".barras");
+const header = document.querySelector(".navbar");
+
+
+barras.addEventListener("click", ()=>
+    header.classList.toggle("ativar")
+);
+const item = document.querySelectorAll(".item");
+
+item.forEach(item => {
+    item.addEventListener("click", () => {
+      header.classList.remove("ativar");
+      
+    });
+  });
+
+  window.addEventListener('scroll', function() {
+    const check = document.querySelectorAll('input[type="radio"]');
+    check.forEach(radio => {
+        radio.checked = false;
+    });
+});
+
